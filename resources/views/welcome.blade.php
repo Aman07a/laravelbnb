89460b34d695281b976124e504355f11c50fece9
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-21 16:41:26 +0200

    Added Scripts to Welcome Blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>LaravelBnb</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-    <!-- Styles -->
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
 </head>
 
 <body>

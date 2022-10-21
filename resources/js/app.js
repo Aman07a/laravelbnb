c378f8d5121e2080f4d2603d2d6e0443e244e46b
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-21 18:29:10 +0200

    Added Vue.component('example-2') to App JS

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,8 @@ import VueRouter from 'vue-router';
 import router from "./routes";
 
 window.Vue = require("vue");
-Vue.component(
-    'example-component',
-    require('./components/ExampleComponent.vue').default
-);
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('example-2', require('./components/Example2.vue').default);
 
 Vue.use(VueRouter);
 

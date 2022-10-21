5780fb6cdffef2e895c850fa05f65e812c52967b
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-21 18:33:38 +0200

    Added Index to App JS

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,15 +2,17 @@ require('./bootstrap');
 
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Index from "./Index";
 import router from "./routes";
 
 window.Vue = require("vue");
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('example-2', require('./components/Example2.vue').default);
 
 Vue.use(VueRouter);
 
 const app = new Vue({
     el: '#app',
     router,
+    components: {
+        index: Index,
+    },
 });

f0e74c730390e686daf67217d963c2a5f122402e
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-21 20:34:24 +0200

    Reactivity gotchas

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -10,6 +10,11 @@
             :item-content="bookable2.content"
             :price="1500"
         ></bookable-list-item>
+        <bookable-list-item
+            :item-title="bookable3.title"
+            :item-content="bookable3.content"
+            :price="1500"
+        ></bookable-list-item>
     </div>
 </template>
 
@@ -22,14 +27,21 @@ export default {
     },
     data() {
         return {
-            bookable1: {
-                title: "Cheap Villa !!!",
-                content: "A very cheap villa",
-            },
-            bookable2: {
-                title: "Cheap Villa 2",
-                content: "A very cheap villa 2",
-            },
+            bookable1: null,
+            bookable2: null,
+            bookable3: null,
+            // bookable1: {
+            //     title: "Cheap Villa !!!",
+            //     content: "A very cheap villa",
+            // },
+            // bookable2: {
+            //     title: "Cheap Villa 2",
+            //     content: "A very cheap villa 2",
+            // },
+            // bookable3: {
+            //     title: "Cheap Villa 3",
+            //     content: "A very cheap villa 3",
+            // },
         };
     },
     // beforeCreate() {
@@ -39,11 +51,32 @@ export default {
         console.log("created");
         console.log(this.bookable1);
         console.log(this.bookable2);
+        console.log(this.bookable3);
 
         setTimeout(() => {
-            this.bookable1.title = "Expensive Villa";
-            this.bookable2.title = "Very Expensive Villa";
+            this.bookable1 = {
+                title: "Cheap Villa !!!",
+                content: "A very cheap villa",
+            };
+            this.bookable2 = {
+                title: "Cheap Villa 2",
+                content: "A very cheap villa 2",
+            };
+            this.bookable3 = {
+                title: "Expensive villa",
+                content: "A very cheap villa 3",
+            };
         }, 5000);
+
+        setTimeout(() => {
+            console.log("First change");
+            this.bookable1.title = "You will see this!";
+        }, 8000);
+
+        setTimeout(() => {
+            console.log("Second change");
+            this.bookable3.title = "You wont see this!";
+        }, 12000);
     },
     // beforeMount() {
     //     console.log("before mount");

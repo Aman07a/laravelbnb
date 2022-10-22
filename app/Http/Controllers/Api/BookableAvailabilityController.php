113a3d73da64e35df7206ac8f036b179730860b8
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-22 20:13:23 +0200

    Returning bookable availability

--- app/Http/Controllers/Api/BookableAvailabilityController.php
+++ app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -11,18 +11,19 @@ class BookableAvailabilityController extends Controller
     /**
      * Handle the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function __invoke(Request $request, $id)
     {
         $data = $request->validate([
             'from' => 'required|date_format:Y-m-d|after_or_equal:now',
-            'to' => 'required|date_format:Y-m-d|after_or_equal:from'
+            'to' => 'required|date_format:Y-m-d|after_or_equal:from',
         ]);
 
         $bookable = Bookable::findOrFail($id);
 
-        dd($bookable->bookings()->betweenDates($data['from'], $data['to'])->count());
+        return $bookable->availableFor($data['from'], $data['to'])
+            ? response()->json([])
+            : response()->json([], 404);
     }
 }
